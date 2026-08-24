9c9d46073b728f3704514460fc5eec59d6a7aa75
Rebin <rebinjoachim@gmail.com>
2026-08-24 22:50:47 +0800

    Updated the tabBar UI

--- src/app/pages/_layout.tsx
+++ src/app/pages/_layout.tsx
@@ -1,10 +1,36 @@
+import Ionicons from '@react-native-vector-icons/ionicons';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout(){
     return(
-        <Tabs>
-            <Tabs.Screen name='homepage' options={{title: 'Home', headerShown: false}} />
-            <Tabs.Screen name='about' options={{title: 'About', headerShown: false}} />
+        <Tabs screenOptions={{
+            tabBarActiveTintColor: '#03c1f5',
+            tabBarInactiveTintColor: '#fff',
+            headerShadowVisible: false,
+            headerTintColor: '#fff',
+            tabBarStyle: {
+                backgroundColor: '#080808',
+            },
+        }}>
+            <Tabs.Screen 
+                name='homepage' 
+                options={{
+                    title: 'Home', 
+                    headerShown: false,
+                    tabBarIcon: ({color, focused}) => (
+                        <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+                    )
+                }} />
+
+            <Tabs.Screen 
+                name='about' 
+                options={{
+                    title: 'About', 
+                    headerShown: false,
+                    tabBarIcon: ({color, focused}) => (
+                        <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
+                    )
+                }} />
         </Tabs>
     )
 }
