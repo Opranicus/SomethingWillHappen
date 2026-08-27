7a4f05bc900330e241cda3e65a039d546375101f
Rebin <rebinjoachim@gmail.com>
2026-08-27 10:56:47 +0800

    Updated the input styling

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,4 +1,4 @@
-import { Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 type Props = {
     label: string;
@@ -8,12 +8,34 @@ type Props = {
 
 export default function Input({label, value, onChange} : Props){
     return(
-        <View>
-            <Text>{label}</Text>
-            <TextInput 
+        <View style={styles.container}>
+            <Text style={styles.inputLabel}>{label}:</Text>
+            <TextInput
+                style={styles.input} 
                 value={value}
                 onChangeText={onChange}
             />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10,
+    },
+
+    inputLabel: {
+        fontSize: 22,
+        color: "#fff",
+        fontWeight: 'bold',
+    },
+
+    input: {
+        width: '100%',
+        padding: 10,
+        color: '#03c1f5'
+    }
+})
