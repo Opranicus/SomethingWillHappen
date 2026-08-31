759e994f288a8e21cc2a4f162c05589ce8e9238e
Rebin <rebinjoachim@gmail.com>
2026-08-31 15:53:59 +0800

    Added the edit and delete buttons

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 type Props = {
     label: string;
     children?: React.ReactNode;
+    update?: () => void;
+    remove? : () => void;
 }
 
-export default function Cards({label, children} : Props){
-    return(
+export default function Cards({ label, children, update, remove }: Props) {
+    return (
         <View style={styles.card}>
             <Text style={styles.text}>{label}</Text>
             {children}
+            <View style={styles.btnContainer}>
+                <Pressable
+                    style={styles.editBtn}
+                    onPress={update}
+                >
+                    <Text style={styles.btnLabel}>Edit</Text>
+                </Pressable>
+
+                <Pressable
+                    style={styles.delBtn}
+                    onPress={remove}
+                >
+                    <Text style={styles.btnLabel}>Delete</Text>
+                </Pressable>
+            </View>
+
         </View>
     )
 }
@@ -24,6 +42,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#fff',
         padding: 10,
+
     },
 
     text: {
@@ -31,5 +50,40 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
         marginTop: 10,
+    },
+
+    editBtn: {
+        backgroundColor: '#1c18a7',
+        width: 50,
+        padding: 5,
+        borderRadius: 10,
+        borderWidth: 3,
+        borderColor: "#080808",
+        textAlign: 'center',
+    },
+
+    delBtn: {
+        backgroundColor: '#dc1313',
+        width: 60,
+        padding: 5,
+        borderRadius: 10,
+        borderWidth: 3,
+        borderColor: "#080808",
+        textAlign: 'center',
+    },
+
+    
+
+    btnLabel: {
+        color: '#fff',
+        textAlign: 'center',
+    },
+
+    btnContainer: {
+        flex: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        gap: 10,
     }
 })
